c7b83d9f35a21647a2622cd617459483c3a91fe0
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    add from_vec(), Transformations, etc

--- src/models.jl
+++ src/models.jl
@@ -2,6 +2,25 @@ const UVType = StaticVector{2}
 const XYType = StaticVector{2}
 
 
+module Transformations
+
+abstract type Transformation end
+function transform end
+
+struct Identity <: Transformation end
+transform(tr::Identity, x, ::Val) = x
+
+Base.@kwdef struct FuncTransformation{T1, T2, T3} <: Transformation
+	flux::T1
+	σ::T2
+	x::T3
+end
+transform(tr::FuncTransformation, x, ::Val{:flux}) = tr.flux(x)
+transform(tr::FuncTransformation, x, ::Val{:σ}) = tr.σ(x)
+transform(tr::FuncTransformation, x, ::Val{:coords}) = (tr.x(x[1]), tr.x(x[2]))
+end
+
+
 abstract type ModelComponent end
 
 position_angle((c_from, c_to)::Pair{<:ModelComponent, <:ModelComponent}) = atan( (coords(c_to) - coords(c_from))... )
@@ -40,6 +59,17 @@ Base.@kwdef struct CircularGaussian{TF,TC} <: ModelComponent
     σ::TC
     coords::SVector{2, TC}
 end
+
+n_params(::Type{<:CircularGaussian}) = 4
+function from_vec(::Type{<:CircularGaussian}, x::AbstractVector, start=1)
+    CircularGaussian(x[start], x[start+1], SVector(x[start+2], x[start+3]))
+end
+function transform_vec!(::Type{<:CircularGaussian}, tr::Transformations.Transformation, x::AbstractVector, start=1)
+    x[start] = Transformations.transform(tr, x[start], Val(:flux))
+    x[start+1] = Transformations.transform(tr, x[start+1], Val(:σ))
+    x[start+2], x[start+3] = Transformations.transform(tr, SVector(x[start+2], x[start+3]), Val(:coords))
+end
+param_names(::Type{<:CircularGaussian}) = ["flux", "σ", "x", "y"]
 
 fwhm_max(c::CircularGaussian) = fwhm_average(c)
 fwhm_min(c::CircularGaussian) = fwhm_average(c)
@@ -83,6 +113,31 @@ end
 
 components(m::MultiComponentModel) = m.components
 visibility(model::MultiComponentModel, uv::UVType) = sum(c -> visibility(c, uv), components(model))
+
+@generated n_params(::Type{MultiComponentModel{TUP}}) where {TUP <: Tuple} = sum(n_params(T) for T in fieldtypes(TUP))
+@generated function from_vec(::Type{MultiComponentModel{TUP}}, x::AbstractVector, start=1) where {TUP <: Tuple}
+    types = fieldtypes(TUP)
+    quote
+        @assert length(x) == $(n_params(MultiComponentModel{TUP}))
+        MultiComponentModel(tuple(
+            $([:(from_vec($T, x, start + $(sum(n_params(TT) for TT in types[1:i-1]; init=0)))) for (i, T) in enumerate(types)]...)
+        ))
+    end
+end
+
+@generated function transform_vec!(::Type{MultiComponentModel{TUP}}, tr::Transformations.Transformation, x::AbstractVector, start=1) where {TUP <: Tuple}
+    types = fieldtypes(TUP)
+    quote
+        @assert length(x) == $(n_params(MultiComponentModel{TUP}))
+        $([:(transform_vec!($T, tr, x, start + $(sum(n_params(TT) for TT in types[1:i-1]; init=0)))) for (i, T) in enumerate(types)]...)
+        return x
+    end
+end
+transform_vec(TMOD::Type{<:MultiComponentModel}, tr::Transformations.Transformation, x::AbstractVector) = transform_vec!(TMOD, tr, copy(x))
+
+param_names(::Type{MultiComponentModel{TUP}}) where {TUP <: Tuple} = mapreduce(vcat, fieldtypes(TUP) |> enumerate) do (i, T)
+    ["$(i)_$(n)" for n in param_names(T)]
+end
 
 
 function model_from_difmap(components::AbstractVector)
